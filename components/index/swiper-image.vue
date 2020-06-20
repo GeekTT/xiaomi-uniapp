7e5f8843b56e6db4556f6762236688fb328efb00
JakeZhang <name>
2020-06-20 10:03:37 +0800

    project init

---
+++ components/index/swiper-image.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="">
+		<swiper indicator-dots autoplay circular :interval="3000" :duration="1000">
+			<block v-for="(item,index) in swiperArr" :key="index">
+				<swiper-item>
+					<image :src="item.src" mode="" lazy-load style="height: 350upx;"></image>
+				</swiper-item>
+			</block>
+			
+		</swiper>
+	</view>
+</template>
+
+<script>
+	export default {
+		props:{
+			swiperArr:Array
+		},
+		data() {
+			return{
+				
+			}
+		},
+		methods: {
+	
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+	<view class="">
+		<swiper-image :swiperArr="swiperArr"></swiper-image>
+	</view>
+</template>
+
+<script>
+	import swiperImage from '../../components/index/swiper-image.vue'
+	export default {
+		components:{
+			swiperImage
+			
+		},
+		data() {
+			return{
+				swiperArr:[
+					{
+						src:'../../static/images/demo/cate_banner.png'
+					},
+					{
+						src:'../../static/images/demo/cate_banner.png'
+					},
+					{
+						src:'../../static/images/demo/cate_banner.png'
+					}
+				]
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
